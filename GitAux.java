b55a6b10af36f5670bbc329bb9e34ea3435c28cd
JulienPanart <JulienPanart@users.noreply.github.com>
2015-03-13 21:23:34 +0100

    Update GitAux.java

--- GitAux.java
+++ GitAux.java
@@ -13,6 +13,7 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+        System.out.println("modificado por el alumno 1"); //alumno 1
     }
     
     public void metodoAlumno2(){
@@ -22,5 +23,8 @@ public class GitAux {
     public void metodoComunitario(){
 		// Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
+        //Modificacion alumno 1
+        System.out.println("Alumno 1 inserta esta sentencia");
+        
     }
 }
